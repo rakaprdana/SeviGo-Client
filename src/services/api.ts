73374263f590503a14d5d8212d621eab7049155d
rakaprdana <raka.pradana1103@gmail.com>
2024-12-13 13:27:15 +0700

    change api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL; // Akses URL dari .env
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://sevigo-api-production.up.railway.app/api"; // Akses URL dari .env
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true,  
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
   (config) => {
-    if(config.data instanceof FormData) {
+    if (config.data instanceof FormData) {
       config.headers["Content-Type"] = "multipart/form-data"; // Tambahkan header untuk form data
     } else {
       config.headers["Content-Type"] = "application/json"; // Tambahkan header
